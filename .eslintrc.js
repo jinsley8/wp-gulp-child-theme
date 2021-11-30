df2bf45d29c5210af20819481a3a35343a3a28ef
Jon Insley <5669143+jinsley8@users.noreply.github.com>
2021-11-30 16:13:07 -0500

    fix: Improve eslint config and add config for prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,30 @@
 module.exports = {
-  env: {
-    browser: true,
-    commonjs: true,
-    es6: true,
-    node: true
-  },
-  extends: ["eslint:recommended", "wordpress"],
-  parserOptions: {
-    sourceType: "module"
-  },
-  rules: {
-    indent: ["error", "tab"],
-    "linebreak-style": ["error", "unix"],
-    quotes: ["error", "single"],
-    semi: ["error", "always"]
-  }
+	env: {
+		browser: true,
+		commonjs: true,
+		es2021: true,
+		node: true,
+		jquery: true,
+	},
+	extends: [
+		"plugin:@wordpress/eslint-plugin/recommended",
+		"plugin:prettier/recommended",
+	],
+	parserOptions: {
+		ecmaVersion: 12,
+		sourceType: "module",
+	},
+	rules: {
+		// Disable weird WP spacing rules.
+		// 'space-before-function-paren': 'off',
+		// 'space-in-parens': 'off',
+		// 'array-bracket-spacing': 'off',
+		indent: ["error", "tab"],
+		semi: ["error", "always"],
+		quotes: ["error", "single"],
+		"linebreak-style": ["error", "unix"],
+		"no-unused-vars": ["warn"],
+		"no-console": "off",
+		"prettier/prettier": "warn",
+	},
 };
